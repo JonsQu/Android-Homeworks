8fb1bb11064edf72d4ad7aceb35e546a4cfb7746
JonsQu <joonatan.haavisto@cs.tamk.fi>
2020-03-26 10:56:49 +0200

    SocialConnection

---
+++ SocialConnection/app/src/main/java/shadow/fi/socialconnection/MainActivity.java
@@ -0,0 +1,15 @@
+package shadow.fi.socialconnection;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
